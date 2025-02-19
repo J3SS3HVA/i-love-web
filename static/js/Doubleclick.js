87b41b9e13d34723fd08e156175e6faeefb503b9
J3SS3HVA <jesse.leeuwerink@hva.nl>
2025-02-19 11:52:34 +0100

    removed wrapper function and old function of the color change

--- static/js/Doubleclick.js
+++ static/js/Doubleclick.js
@@ -1,4 +1,4 @@
-(function () {
+
     function getRandomColor() {
         return "#" + Math.floor(Math.random() * 16777215).toString(16);
     }
@@ -51,37 +51,3 @@ document.addEventListener("touchend", (event) => {
     }
     lastTouchTime = currentTime;
 });
-})();
-
-
-
-
-
-// <!--permanent standaard kleur-->
-// <!-- 
-// <script>
-//     // Functie om een willekeurige kleur te genereren
-//     function getRandomColor() {
-//         const randomColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
-//         return randomColor;
-//     }
-
-//     // Functie die wordt aangeroepen bij dubbelklikken
-//     function changePrimaryColor() {
-//         const newColor = getRandomColor();
-//         document.documentElement.style.setProperty('--primary-color', newColor);
-//     }
-
-//     // Gebruik een lifecycle hook om de body een event listener toe te voegen
-//     import { onMount } from 'svelte';
-
-//     onMount(() => {
-//         document.body.addEventListener('dblclick', changePrimaryColor);
-        
-//         // Zorg ervoor dat de event listener wordt verwijderd bij unmounting
-//         return () => {
-//             document.body.removeEventListener('dblclick', changePrimaryColor);
-//         };
-//     });
-// </script>
-//  -->
